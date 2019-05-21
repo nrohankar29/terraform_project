4265fe5d045eb04fef4fa733a672e0a61fa63652
Nikhil Rohankar <nikhil.rohankar@hitachivantara.com>
2019-05-21 10:28:42 +0530

    updated main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 provider "aws" {
-  region = "us-east-1"
+  region                  = "us-east-1"
+  shared_credentials_file = "/opt/tomcat/.aws/credentials"
+  profile                 = "devops"
 }
-
 resource "aws_instance" "web" {
   ami           = "ami-0756fbca465a59a30"
   instance_type = "t2.micro"
